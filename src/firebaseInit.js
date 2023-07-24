a73577f5dc3ec486f3a80fb186c49d11bd002748
Lakshay Gupta <89699100+lakshay-9818@users.noreply.github.com>
2023-07-25 00:23:54 +0530

    trial

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -5,12 +5,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID   
+  // apiKey: process.env.REACT_APP_APIKEY,
+  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  // projectId: process.env.REACT_APP_PROJECTID,
+  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  // appId: process.env.REACT_APP_APPID 
+  apiKey: "AIzaSyAVDWWqCfJADYMVSiZ_1udRtgukRRgdwQs",
+  authDomain: "expense-tracker-cafdb.firebaseapp.com",
+  projectId: "expense-tracker-cafdb",
+  storageBucket: "expense-tracker-cafdb.appspot.com",
+  messagingSenderId: "1065185125006",
+  appId: "1:1065185125006:web:7606683d6233524e329fdf"
 };
 
 const app = initializeApp(firebaseConfig);
